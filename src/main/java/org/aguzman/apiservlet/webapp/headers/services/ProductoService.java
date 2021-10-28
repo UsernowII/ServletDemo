4e4e363139c6d3e8ce4c260fde35f1e66cf1ebaf
Erick santos <usernow.82@gmail.com>
2021-10-28 18:23:25 -0500

    Web headers

---
+++ src/main/java/org/aguzman/apiservlet/webapp/headers/services/ProductoService.java
@@ -0,0 +1,11 @@
+package org.aguzman.apiservlet.webapp.headers.services;
+
+import org.aguzman.apiservlet.webapp.headers.model.Producto;
+
+
+import java.util.List;
+
+public interface ProductoService {
+    List<Producto> listar();
+
+}
